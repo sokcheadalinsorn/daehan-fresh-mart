5f15ab98681975e8fa5bc8c006f06dca5f66c1af
sokcheadalinsorn <sokcheadalinsorn@gmail.com>
2025-12-16 16:50:41 +0700

    do  homepage and products

--- wp-content/themes/my-ecommerce-theme/footer.php
+++ wp-content/themes/my-ecommerce-theme/footer.php
@@ -1,50 +1,47 @@
 </main>
-<footer class="footer">
-<div class="footer-top">
-<div class="footer-col">
-<h3>Quick links</h3>
-<ul>
-<li><a href="#">Home</a></li>
-<li><a href="#">Product</a></li>
-<li><a href="#">Contact us</a></li>
-<li><a href="#">About us</a></li>
-</ul>
-</div>
+<footer>
 
+    <div class="left-start-footer">
 
-<div class="footer-col">
-<h3>Contact us</h3>
-<ul class="contact">
-<li><i class="fa-solid fa-paper-plane"></i> Daehan freah mart</li>
-<li><i class="fa-brands fa-instagram"></i> Daehan freah mart</li>
-<li><i class="fa-brands fa-facebook"></i>Daehan freah mart</li>
-<li><i class="fa-brands fa-youtube"></i> Daehan freah mart</li>
-</ul>
-</div>
+        <div class="map-and-text">
+            <div class="footer-text-daehan-fresh-mart">
+                <p class="text-daehan-fresh-mart"><?php echo date('Y'); ?> Daehan Fresh Mart (대한프레시마트). </p>
+            </div>
 
+            <div class="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15636.800440411322!2d104.8805006976919!3d11.537498285584194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x310950549f27fc1b%3A0x65c2d5b055de0d9d!2sPSE%20-%20Pour%20un%20Sourire%20d&#39;Enfant!5e0!3m2!1sen!2skh!4v1765782940549!5m2!1sen!2skh" width="700" height="200" margin="auto" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="-map-"></iframe>
+            </div>
+        </div>
+        
+        <div class="footer-contactAs-home-about">
+            <div class=" footer-text-home-about">
+                <h3 class="text-home-about">Contact</h3>
+                <div class="--text-home-about--">
+                        <p href="" class="--text-home-about-"><i class="fa-brands fa-facebook"></i>  Facebook</p>
+                        <p href="" class="--text-home-about-"><i class="fa-brands fa-tiktok"></i>  Tick Tock</p>
+                        <p href=""class="--text-home-about-"><i class="fa-brands fa-instagram"></i>  Instargram</p>
+                </div>  
+            </div>
+            <div class=" footer-text-home-about">
+                <h3 class="text-home-about">Campany</h3>
+                <div class="--text-home-about--">
+                        <p href="" class="--text-home-about-">Home</p>
+                        <p href="" class="--text-home-about-">About</p>
+                        <p href="" class="--text-home-about-">contact as</p>
+                </div>  
+            </div>
+        </div>
 
-<div class="footer-col">
-<h3>Location</h3>
-<p>#402, Threa village,<br />
-stung Meanchey,<br />
-Meanchey, Phnom Penh</p>
-</div>
-</div>
+    </div>    
+        
 
+    <div class="footer-line"></div>
 
-<div class="footer-middle">
-<span><i class="fa-solid fa-phone"></i> 0887557692</span>
-<span><i class="fa-regular fa-clock"></i> 7 days a week, from 10am to 8pm</span>
-</div>
+    <div class="footer-end-text-DaehanFreshMart">
+        <h1 class="footer-text-h1">Daehan Fresh Mart.2025<h1>
+    </div>
 
-
-<div class="footer-bottom">
-<p>©2025 All rights reserved. Daehan fresh Mart</p>
-</div>
 </footer>
-
-
-</body>
-</html>
 <?php wp_footer(); ?>
-</body></html>
+    </body>
+</html>
--- wp-content/themes/my-ecommerce-theme/front-page.php
+++ wp-content/themes/my-ecommerce-theme/front-page.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
 
 <!-- hero_section -->
 <div class="container">
@@ -12,9 +13,7 @@
             distinctio ad nulla nesciunt ullam dolores maxime exercitationem <br>
             natus repudiandae dicta minima facilis earum iure totam?
         </p>
-        <div class="btn">
-            <button type="button" onclick="alert('Hello world!')">SHOP NOW</button>
-        </div>
+        <button class="btn" onclick=>SHOW ME</button>
     </div>
     <div class="pic">
         <img src="http://daehan-fresh-mart.test/wp-content/uploads/2025/12/photo_6170453367138749477_x-removebg-preview.png" alt="pic">
@@ -62,31 +61,287 @@
 </div>
 
 <!-- feature_cart_items -->
-<div class="feature_title">
+<div class="feature">
     <h2>Feature Products</h2>
     <p class="view_more">View More</p>
 </div> 
-<div class="feature_cart_items">
-    <div class="pic_categories">
-        <img src="http://daehan-fresh-mart.test/wp-content/uploads/2025/12/mushroom-removebg-preview.png" alt="pic_categores">
-        <p class="name_categories">Mushroom</p>
+
+<div class="feature">
+
+    <div class="feature_cart_items">
+
+        <div class="product-card">
+
+            <span class="badge">NEW</span>
+            <span class="heart"><i class="fa-regular fa-heart" style="color: #000000;"></i></span>
+
+            <img src="http://daehan-fresh-mart.test/wp-content/uploads/2025/12/cabbage-removebg-preview1.png" alt="Cabbage">
+
+            <p class="category">Vegetable <span class="rating"><i class="fa-solid fa-star" style="color: #f5c211;"></i>4.00</span></p>
+
+            <h3 class="title">Cabbage</h3>
+            <p class="stock">In stock : 50/100</p>
+
+            <div class="price">
+
+               <span class="current">$5.00</span>
+               <span class="weight">250g</span>
+            </div>
+
+            <div class="qty">
+               <button>-</button>
+               <span>2</span>
+               <button class="plus">+</button>
+            </div>
+
+            <div class="actions">
+               <button class="buy">BUY NOW</button>
+               <button class="add"><i class="fa-solid fa-cart-shopping" style="color: #000000;"></i>Add</button>
+            </div>
+        </div>
+
     </div>
 
-    <div class="pic_categories">
-        <img src="http://daehan-fresh-mart.test/wp-content/uploads/2025/12/mushroom-removebg-preview.png" alt="pic_categores">
-        <p class="name_categories">Mushroom</p>
+    <div class="feature_cart_items">
+
+        <div class="product-card">
+
+            <span class="badge">NEW</span>
+            <span class="heart"><i class="fa-regular fa-heart" style="color: #000000;"></i></span>
+
+            <img src="http://daehan-fresh-mart.test/wp-content/uploads/2025/12/cabbage-removebg-preview1.png" alt="Cabbage">
+
+            <p class="category">Vegetable <span class="rating"><i class="fa-solid fa-star" style="color: #f5c211;"></i>4.00</span></p>
+
+            <h3 class="title">Cabbage</h3>
+            <p class="stock">In stock : 50/100</p>
+
+            <div class="price">
+
+               <span class="current">$5.00</span>
+               <span class="weight">250g</span>
+            </div>
+
+            <div class="qty">
+               <button>-</button>
+               <span>2</span>
+               <button class="plus">+</button>
+            </div>
+
+            <div class="actions">
+               <button class="buy">BUY NOW</button>
+               <button class="add"><i class="fa-solid fa-cart-shopping" style="color: #000000;"></i> Add</button>
+            </div>
+        </div>
+
     </div>
 
-     <div class="pic_categories">
-        <img src="http://daehan-fresh-mart.test/wp-content/uploads/2025/12/mushroom-removebg-preview.png" alt="pic_categores">
-        <p class="name_categories">Mushroom</p>
+    <div class="feature_cart_items">
+
+        <div class="product-card">
+
+            <span class="badge">NEW</span>
+            <span class="heart"><i class="fa-regular fa-heart" style="color: #000000;"></i></span>
+
+            <img src="http://daehan-fresh-mart.test/wp-content/uploads/2025/12/cabbage-removebg-preview1.png" alt="Cabbage">
+
+            <p class="category">Vegetable <span class="rating"><i class="fa-solid fa-star" style="color: #f5c211;"></i> 4.00</span></p>
+
+            <h3 class="title">Cabbage</h3>
+            <p class="stock">In stock : 50/100</p>
+
+            <div class="price">
+
+               <span class="current">$5.00</span>
+               <span class="weight">250g</span>
+            </div>
+
+            <div class="qty">
+               <button>-</button>
+               <span>2</span>
+               <button class="plus">+</button>
+            </div>
+
+            <div class="actions">
+               <button class="buy">BUY NOW</button>
+               <button class="add"><i class="fa-solid fa-cart-shopping" style="color: #000000;"></i> Add</button>
+            </div>
+        </div>
+
     </div>
 
-    
+    <div class="feature_cart_items">
 
-    
+        <div class="product-card">
+
+            <span class="badge">NEW</span>
+            <span class="heart"><i class="fa-regular fa-heart" style="color: #000000;"></i></span>
+
+            <img src="http://daehan-fresh-mart.test/wp-content/uploads/2025/12/cabbage-removebg-preview1.png" alt="Cabbage">
+
+            <p class="category">Vegetable <span class="rating"><i class="fa-solid fa-star" style="color: #f5c211;"></i>4.00</span></p>
+
+            <h3 class="title">Cabbage</h3>
+            <p class="stock">In stock : 50/100</p>
+
+            <div class="price">
+
+               <span class="current">$5.00</span>
+               <span class="weight">250g</span>
+            </div>
+
+            <div class="qty">
+               <button>-</button>
+               <span>2</span>
+               <button class="plus">+</button>
+            </div>
+
+            <div class="actions">
+               <button class="buy">BUY NOW</button>
+               <button class="add"><i class="fa-solid fa-cart-shopping" style="color: #000000;"></i> Add</button>
+            </div>
+        </div>
+
+    </div>
+
+     <div class="feature_cart_items">
+
+        <div class="product-card">
+
+            <span class="badge">NEW</span>
+            <span class="heart"><i class="fa-regular fa-heart" style="color: #000000;"></i></span>
+
+            <img src="http://daehan-fresh-mart.test/wp-content/uploads/2025/12/cabbage-removebg-preview1.png" alt="Cabbage">
+
+            <p class="category">Vegetable <span class="rating"><i class="fa-solid fa-star" style="color: #f5c211;"></i> 4.00</span></p>
+
+            <h3 class="title">Cabbage</h3>
+            <p class="stock">In stock : 50/100</p>
+
+            <div class="price">
+
+               <span class="current">$5.00</span>
+               <span class="weight">250g</span>
+            </div>
+
+            <div class="qty">
+               <button>-</button>
+               <span>2</span>
+               <button class="plus">+</button>
+            </div>
+
+            <div class="actions">
+               <button class="buy">BUY NOW</button>
+               <button class="add"><i class="fa-solid fa-cart-shopping" style="color: #000000;"></i> Add</button>
+            </div>
+        </div>
+
+    </div>
 
 </div>
+
+<!-- discout_section_1 -->
+<div class="discount_section">
+    <div class="pic-discount">
+        <img src="http://daehan-fresh-mart.test/wp-content/uploads/2025/12/photo_6172705166952434511_x-removebg-preview.png" alt="pic_discount">
+    </div>
+    <div class="description">
+        <button class="btn_discount" onclick=>20% Discount</button>
+        <h1 class="tiltle_discount">Purely Fresh Vegetables</h1>
+        <p class="p_discount">come to get a good product for your health</p>
+        <button class="btn_buy_now" onclick=>BUY NOW</button>
+    </div>
+</div>
+
+<!-- service_section -->
+<div class="service">
+    <img src="http://daehan-fresh-mart.test/wp-content/uploads/2025/12/photo_6163370759909215459_y_2_-removebg-preview1.png" alt="logo">
+    <h1 class="service_title">Our Service</h1>
+</div>
+
+<!-- delivery_section -->
+<div class="-delivery_section-">
+    <div class="delivery_section">
+        <div class="pic_delivery">
+            <img src="http://daehan-fresh-mart.test/wp-content/uploads/2025/12/photo_6170453367138749549_x-removebg-preview.png" alt="pic-delivery">
+        </div>
+        <div class="description">
+            <h2>Delivery</h2>
+            <p>“We deliver fresh vegetables directly from <br>
+                trusted farms to your home. Every order is packed with care to keep your <br>
+                veggies crisp, clean, and full of flavor. Enjoy fast delivery and the convenience of shopping from home.”
+            </p>
+            <div class="img">
+                <img src="http://daehan-fresh-mart.test/wp-content/uploads/2025/12/photo_6170453367138749480_x-removebg-preview.png" alt="">
+            </div>
+        </div>
+    </div>
+
+</div>
+ 
+<!-- farmer__section -->
+ <!-- sechtion-discount -->
+    <section>
+    <!--Awesome--farmers-->
+    <div class="--Awesome--farmers--">
+        <h1 class="Awesome--farmers-text">Awesome farmers tea
+            here to help you
+        </h1>
+    </div>
+    <!--sechtion-->
+        <div class="section-Awesome--farmers-text">
+            <!--Awesome--farmers-1-->
+            <div class="--Awesome--farmers-1--">
+                <div class="--Awesome--farmers-1-photo--">
+                    <img src="http://daehan-fresh-mart.test/wp-content/uploads/2025/12/photo_6172705166952434502_x-removebg-preview.png" class="-Awesome--farmers-1-photo-">
+                </div>
+                <div class="--Awesome--farmers-1-text--">
+                    <div class="box-Awesome--farmers-1-text-p">
+                        <p class="---Awesome--farmers-1-text-p--"><b>Name</b>:  Neary Pichy </p>
+                        <p class="---Awesome--farmers-1-text-p--"><b>Telegram</b>:  096949593 </p>
+                        <p class="---Awesome--farmers-1-text-p--"><b>Location</b>:  Phum Prek Thom,    Songkat Kbal Koh ,Phnom Penh. </p>
+                    </div>                      
+                </div>
+            </div>
+            <!--Awesome--farmers-2-->
+            <div class="--Awesome--farmers-2--">
+                <div class="--Awesome--farmers-1-photo--">
+                    <img src="http://daehan-fresh-mart.test/wp-content/uploads/2025/12/photo_6172705166952434508_x-removebg-preview.png" class="-Awesome--farmers-1-photo-">
+                </div>
+                <div class="--Awesome--farmers-1-text--">
+                    <div class="box-Awesome--farmers-1-text-p">
+                        <p class="---Awesome--farmers-1-text-p--"><b>Name</b>:  Ling Koling</p>
+                        <p class="---Awesome--farmers-1-text-p--"><b>Telegram</b>:  096949593 </p>
+                        <p class="---Awesome--farmers-1-text-p--"><b>Location</b>:  Phum Prek Thom,    Songkat Kbal Koh ,Phnom Penh. </p>
+                    </div>                      
+                </div>
+            </div>
+            <!--Awesome--farmers-3-->
+            <div class="--Awesome--farmers-3--">
+                <div class="--Awesome--farmers-1-photo--">
+                    <img src="http://daehan-fresh-mart.test/wp-content/uploads/2025/12/photo_6172705166952434507_x-removebg-preview.png" class="-Awesome--farmers-1-photo-">
+                </div>
+                <div class="--Awesome--farmers-1-text--">
+                    <div class="box-Awesome--farmers-1-text-p">
+                        <p class="---Awesome--farmers-1-text-p--"><b>Name</b>:  Neary Pichy </p>
+                        <p class="---Awesome--farmers-1-text-p--"><b>Telegram</b>:  096949593 </p>
+                        <p class="---Awesome--farmers-1-text-p--"><b>Location</b>:  Phum Prek Thom,    Songkat Kbal Koh ,Phnom Penh. </p>
+                    </div>                      
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+  
+
+    
+
+    
+
+
 
 
 
